8be5285997938098187eed8ad4b0f7a670a6e4ab
ljs015 <ljs015@latech.edu>
2012-04-26 13:38:29 -0500

    refactored preliminary file structure; put an executable file in bin

---
+++ lib/Asm/Application.rb
@@ -0,0 +1,9 @@
+# this file is undocumented, haha.
+require	'Asm/require_all.rb'
+
+# module Asm contains code relevant to the function of a BCPU VM.
+module Asm
+	# placeholder for an application object class; will handle GUI details.
+	class	Application
+	end
+end
--- lib/asm/version.rb
+++
@@ -1,4 +0,0 @@
-module Asm
-    # http://semver.org/spec/v1.0.0.html
-    VERSION = "0.1.0"
-end
