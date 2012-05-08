559445f6af3dd8f9cbb66a0fa1c6ed8398be9e8d
Delvin Jackson <delJacks@gmail.com>
2012-05-07 22:54:26 -0500

    Completed Instructions in Virtual Machine

--- lib/Asm/Boilerplate.rb
+++ lib/Asm/Boilerplate.rb
@@ -15,7 +15,7 @@ module Asm
 		module Machine
 			module Code
 				# DOCIT
-				def self.get_OPcode_location( a_Memory_Value )
+				def self.get_OPcode_As_String( a_Memory_Value )
 				end
 				# DOCIT
 				def self.get_RD_location( a_Memory_Value )
